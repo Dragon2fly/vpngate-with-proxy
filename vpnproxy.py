5e94c404d2da41fdd4d8643ebc70a744e2a403bb
duc_tin <nguyenbaduc.tin@gmail.com>
2015-04-04 16:09:54 +0900

    First file that can closely meet the expectation

--- vpnproxy.py
+++ vpnproxy.py
@@ -9,8 +9,6 @@ import requests
 import base64
 import time
 import datetime
-import inspect
-import ntpath
 from config import *
 from subprocess import call
 
@@ -21,7 +19,7 @@ class Server():
     def __init__(self, data):
         self.ip = data[1]
         self.score = data[2]
-        self.ping = int(data[3]) if data[3]!='-' else 'inf'
+        self.ping = int(data[3]) if data[3] != '-' else 'inf'
         self.speed = int(data[4])
         self.country = data[6]
         self.NumSessions = data[7]
@@ -57,13 +55,13 @@ def get_data():
         print 'Failed to get VPN servers data\nCheck your network setting and proxy'
 
 # proxy server and port
-path = os.path.abspath(inspect.getsourcefile(get_data))
-config_file = ntpath.split(path)[0] + '/config.ini'
+path = os.path.realpath(sys.argv[0])
+config_file = os.path.split(path)[0] + '/config.ini'
 
 if os.path.exists(config_file):
     proxy, port, sort_by = read_config(config_file)
 else:
-    proxy, port = raw_input(' Enter your http proxy (eg: www.proxy.com:8080 or 123.11.22.33:8080): ').split(':')
+    proxy, port = raw_input(' Enter your http proxy:port\n(eg: www.proxy.com:8080 or 123.11.22.33:8080): ').split(':')
     sort_by = raw_input('sort result by (speed (default) | ping | score):')
     if sort_by not in ['speed', 'ping', 'score']:
         sort_by = 'speed'
