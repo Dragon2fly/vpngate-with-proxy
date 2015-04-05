904228198659c3fd14ee4711cede40993ed3d98c
duc_tin <nguyenbaduc.tin@gmail.com>
2015-04-05 18:31:35 +0900

    add protocol to vpn table

--- config.py
+++ config.py
@@ -2,43 +2,60 @@
 __author__ = 'duc_tin'
 
 import ConfigParser
+import re
 
 
 def get_input(config_path, option):
     if option[0] in ['config']:
-        proxy, port, sort_by = read_config(config_path)
+        proxy, port, sort_by, use_proxy = read_config(config_path)
         print '_Current settings:'
         print '___proxy: %s:%s' % (proxy, port)
         print '___sort servers by: ', sort_by
+        print 'Connect to VPN through proxy: ', use_proxy
 
         while 1:
             print '\nPress Enter to process to vpn list or \n1: to change proxy\'s address\n2: to change proxy\'s ' \
-                  'port\n3: to change sorting parameter'
+                  'port\n3: to change sorting parameter\n4: to turn on/off proxy'
             user_input = raw_input()
             if user_input == '':
                 print 'Process to vpn server list'
-                write_config(config_path, proxy, port, sort_by)
+                write_config(config_path, proxy, port, sort_by, use_proxy)
                 return
             elif user_input == '1':
                 proxy = raw_input('Http proxy\'s address (eg: www.proxy.com or 123.11.22.33): ')
             elif user_input == '2':
-                port = raw_input('Http proxy\'s port (eg: 8080): ')
+                user_input = 'abc'
+                while not re.findall(r'^\d+$', user_input.strip()):
+                    user_input = raw_input('Http proxy\'s port (eg: 8080): ')
+                port = user_input
             elif user_input == '3':
-                sort_by = raw_input('sort result by (speed | ping | score | up time): ')
+                while user_input not in ['speed', 'ping', 'score', 'up time', 'uptime']:
+                    user_input = raw_input('sort result by (speed | ping | score | up time): ')
+                sort_by = 'up time' if user_input == 'uptime' else user_input
+
+            elif user_input == '4':
+                while user_input.lower() not in ['y', 'n', 'yes', 'no']:
+                    user_input = raw_input('Use proxy to connect to vpn? (yes|no): ')
+                else:
+                    use_proxy = 'no' if user_input in 'no' else 'yes'
+            else:
+                print 'Invalid input'
 
 
 def read_config(config_path):
     parser = ConfigParser.SafeConfigParser()
     parser.read(config_path)
+    use_proxy = parser.get('proxy', 'use proxy')
     proxy = parser.get('proxy', 'address')
     port = parser.get('proxy', 'port')
     sort_by = parser.get('sort', 'key')
-    return proxy, port, sort_by
+    return proxy, port, sort_by, use_proxy
 
 
-def write_config(config_path, proxy, port, parameter):
+def write_config(config_path, proxy, port, parameter, use_proxy):
     parser = ConfigParser.SafeConfigParser()
     parser.add_section('proxy')
+    parser.set('proxy', 'use proxy', use_proxy)
     parser.set('proxy', 'address', proxy)
     parser.set('proxy', 'port', port)
 
--- vpnproxy.py
+++ vpnproxy.py
@@ -26,14 +26,16 @@ class Server():
         self.NumSessions = data[7]
         self.uptime = data[8]
         self.logPolicy = data[11]
-        self.config_data = data[-1]
+        self.config_data = base64.b64decode(data[-1])
+        self.proto = 'tcp' if '\r\nproto tcp\r\n' in self.config_data else 'udp'
 
     def write_file(self):
-        txt_data = base64.b64decode(self.config_data)
-        add_proxy = txt_data.replace('\r\n;http-proxy-retry\r\n', '\r\nhttp-proxy-retry 3\r\n')
-        added_proxy = add_proxy.replace('\r\n;http-proxy [proxy server] [proxy port]\r\n',
+        txt_data = self.config_data
+        if use_proxy == 'yes':
+            txt_data = txt_data.replace('\r\n;http-proxy-retry\r\n', '\r\nhttp-proxy-retry 3\r\n')
+            txt_data = txt_data.replace('\r\n;http-proxy [proxy server] [proxy port]\r\n',
                                         '\r\nhttp-proxy %s %s\r\n' % (proxy, port))
-        dns_fixed = added_proxy + Server.dns_leak_stop
+        dns_fixed = txt_data + Server.dns_leak_stop
         tmp_vpn = open('vpn_tmp', 'w+')
         tmp_vpn.write(dns_fixed)
         return tmp_vpn
@@ -42,29 +44,30 @@ class Server():
         speed = self.speed/1000.**2
         uptime = datetime.timedelta(milliseconds=int(self.uptime))
         uptime = re.split(',|\.', str(uptime))[0]
-        txt = [self.country_short, str(self.ping), '%.2f' % speed, uptime, self.logPolicy, str(self.score)]
+        txt = [self.country_short, str(self.ping), '%.2f' % speed, uptime, self.logPolicy, str(self.score), self.proto]
         txt = [dta.center(spaces[ind + 1]) for ind, dta in enumerate(txt)]
         return ''.join(txt)
 
 
 def get_data():
-    proxies = {
-        'http': 'http://' + proxy + ':' + port,
-        'https': 'http://' + proxy + ':' + port,
-    }
+    if use_proxy == 'yes':
+        proxies = {
+            'http': 'http://' + proxy + ':' + port,
+            'https': 'http://' + proxy + ':' + port,
+        }
+    else:
+        proxies = {}
 
     try:
+        print 'Use proxy: ', use_proxy
         vpn_data = requests.get('http://www.vpngate.net/api/iphone/', proxies=proxies, timeout=3).text.replace('\r', '')
         servers = [line.split(',') for line in vpn_data.split('\n')]
         servers = {s[0]: Server(s) for s in servers[2:] if len(s) > 1}
-        print servers.keys()
         return servers
-    except requests.exceptions.ConnectionError:
+    except requests.exceptions.RequestException as e:
+        print e
         print 'Failed to get VPN servers data\nCheck your network setting and proxy'
-        sys.exit()
-    except requests.exceptions.ConnectTimeout:
-        print 'Connection Timeout!\nCheck your network setting and proxy'
-        sys.exit()
+        sys.exit(1)
 
 
 def refresh_data():
@@ -95,22 +98,25 @@ args = sys.argv[1:]
 
 # get proxy from config file
 if os.path.exists(config_file):
-    proxy, port, sort_by = read_config(config_file)
     if len(args):
         # process commandline arguments
         get_input(config_file, args)
+
+    proxy, port, sort_by, use_proxy = read_config(config_file)
+
 else:
+    use_proxy = raw_input('Use proxy to connect to vpn? (yes|no): ')
     proxy, port = raw_input(' Enter your http proxy:port\n(eg: www.proxy.com:8080 or 123.11.22.33:8080): ').split(':')
     sort_by = raw_input('sort result by (speed (default) | ping | score | up time):')
     if sort_by not in ['speed', 'ping', 'score', 'up time']:
         sort_by = 'speed'
 
-    write_config(config_file, proxy, port, sort_by)
+    write_config(config_file, proxy, port, sort_by, use_proxy)
 
 ranked, vpn_list = refresh_data()
 
-labels = ['Index', 'Country', 'Ping', 'Speed', 'Up time', 'Log Policy', 'Score']
-spaces = [6, 7, 6, 10, 10, 10, 10]
+labels = ['Index', 'Country', 'Ping', 'Speed', 'Up time', 'Log Policy', 'Score', 'protocol']
+spaces = [6, 7, 6, 10, 10, 10, 10, 10]
 labels = [label.center(spaces[ind]) for ind, label in enumerate(labels)]
 
 while True:
@@ -119,21 +125,26 @@ while True:
         print '%2d:'.center(6) % index, vpn_list[key]
     try:
         user_input = raw_input('Choose vpn No.: ')
-        if user_input in ['Q', 'q', 'quit', 'exit']:
+        if user_input.strip().lower() in ['q', 'quit', 'exit']:
             sys.exit()
-        elif user_input in ['r', 'R', 'refresh']:
+        elif user_input.strip().lower() in ['r', 'refresh']:
             ranked, vpn_list = refresh_data()
-        elif user_input == 'config':
+        elif user_input.strip().lower() == 'config':
             get_input(config_file, [user_input])
-            proxy, port, sort_by = read_config(config_file)
+            proxy, port, sort_by, use_proxy = read_config(config_file)
             ranked, vpn_list = refresh_data()
-        else:
+        elif re.findall(r'^\d+$', user_input.strip()) and int(user_input) < 20:
             chose = int(user_input)
             print ('Connect to ' + vpn_list[ranked[chose]].country_long).center(40)
             print vpn_list[ranked[chose]].ip.center(40)
             vpn_file = vpn_list[ranked[chose]].write_file()
             vpn_file.close()
             call(['sudo', 'openvpn', '--config', os.path.abspath(vpn_file.name)])
+        else:
+            print 'Invalid command!'
+            print ' q(uit) to quit\n r(efresh) to refresh table\n' \
+                  ' config to change setting\n number in range 0~19 to choose vpn'
+            time.sleep(3)
     except KeyboardInterrupt:
         time.sleep(0.5)
-        print "Select another VPN server or 'q' to quit"
+        print "\nSelect another VPN server or 'q' to quit"
